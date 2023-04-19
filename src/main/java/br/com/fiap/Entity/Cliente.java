0f961e7b086ecd6b82b949df338e8895122c223f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-19 19:45:03 -0300

    add relacao de cliente e pedido

--- src/main/java/br/com/fiap/Entity/Cliente.java
+++ src/main/java/br/com/fiap/Entity/Cliente.java
@@ -1,12 +1,16 @@
 package br.com.fiap.Entity;
 
 import java.util.Calendar;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -30,6 +34,9 @@ public class Cliente {
 	@Temporal(TemporalType.DATE)
 	@Column(name ="dt_nascimento")
 	private Calendar dt_nascimento;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos;
 	
 	public Cliente() {
 		
--- src/main/java/br/com/fiap/Entity/Pedido.java
+++ src/main/java/br/com/fiap/Entity/Pedido.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -33,6 +34,10 @@ public class Pedido {
 	
 	@OneToOne(mappedBy = "pedido")
 	private NotaFiscal nota;
+	
+	@ManyToOne
+	@JoinColumn(name = "cd_cliente", nullable = false)
+	private Cliente cliente;
 	
 	public Pedido() {
 		
